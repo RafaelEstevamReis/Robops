a9a89ec408615b130a809de98c871d9ccd6a5664
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-09-22 01:07:40 -0300

    Create ConversionExtensions.cs

---
+++ Robos.Spiders/Extensions/ConversionExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Robos.Spiders.Extensions
+{
+    public static class ConversionExtensions
+    {
+        public static int ToInt(this string Text)
+        {
+            return int.Parse(Text);
+        }
+    }
+}
