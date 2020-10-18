cd8f6f617728c07364768b855547199c203190cf
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-10-17 23:54:35 -0300

    Coletar deputados candidatos

---
+++ Robops.Lib/Camara.Leg/API/Deputados.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Robops.Lib.Camara.Leg.API
+{
+    public partial class Deputado
+    {
+        public xmlDados dados { get; set; }
+        public xmlLinks[] links { get; set; }
+    }
+
+    public partial class xmlDados
+    {
+        public uint id { get; set; }
+        public string uri { get; set; }
+        public string nomeCivil { get; set; }
+        public xmlDadosUltimoStatus ultimoStatus { get; set; }
+        public string cpf { get; set; }
+        public string sexo { get; set; }
+        public string urlWebsite { get; set; }
+
+        [System.Xml.Serialization.XmlArrayItemAttribute("redeSocial", IsNullable = false)]
+        public string[] redeSocial { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public DateTime? dataFalecimento { get; set; }
+        public string ufNascimento { get; set; }
+        public string municipioNascimento { get; set; }
+        public string escolaridade { get; set; }
+    }
+
+    public partial class xmlDadosUltimoStatus
+    {
+        public uint id { get; set; }
+        public string uri { get; set; }
+        public string nome { get; set; }
+        public string siglaPartido { get; set; }
+        public string uriPartido { get; set; }
+        public string siglaUf { get; set; }
+        public byte idLegislatura { get; set; }
+        public string urlFoto { get; set; }
+        public string email { get; set; }
+        public string data { get; set; }
+        public string nomeEleitoral { get; set; }
+        public xmlDadosUltimoStatusGabinete gabinete { get; set; }
+        public string situacao { get; set; }
+        public string condicaoEleitoral { get; set; }
+        public object descricaoStatus { get; set; }
+    }
+
+    public partial class xmlDadosUltimoStatusGabinete
+    {
+        public string nome { get; set; }
+        public string predio { get; set; }
+        public string sala { get; set; }
+        public string andar { get; set; }
+        public string telefone { get; set; }
+        public string email { get; set; }
+    }
+
+    public partial class xmlLinks
+    {
+        public string rel { get; set; }
+        public string href { get; set; }
+    }
+}
--- RobopsExec/Program.cs
+++ RobopsExec/Program.cs
@@ -1,5 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using Net.RafaelEstevam.Spider.Helper;
+using Newtonsoft.Json;
+using Robops.Lib.Camara.Leg.API;
 using Robops.Spiders.Camara.Leg.Cota;
 
 namespace RobopsExec
@@ -8,6 +16,58 @@ namespace RobopsExec
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Lendo deputados");
+
+            var deputados = Directory
+                .GetFiles(@"B:\Temp\api\deputados\", "*.json")
+                .Select(f => JsonConvert.DeserializeObject<Deputado>(File.ReadAllText(f)))
+                .ToDictionary(o => o.dados.cpf);
+
+            // http://agencia.tse.jus.br/estatistica/sead/odsele/consulta_cand/consulta_cand_2020.zip -> Brasil.csv
+            var arquivo = @"B:\Temp\consulta_cand_2020_BRASIL.csv";
+            using var fs = File.OpenRead(arquivo);
+            var lines = CSVHelper.FileSplit(new StreamReader(fs, Encoding.UTF7))
+                                 .Skip(1);
+
+            Console.WriteLine("Lendo candidatos");
+
+            foreach (var line in lines)
+            {
+                // [20] CPF
+                var cpf = line[20];
+
+                if (deputados.ContainsKey(cpf))
+                {
+                    var deputado = deputados[cpf];
+                    Console.WriteLine($"{deputado.dados.cpf} {deputado.dados.nomeCivil} -> {line[12]}/{line[14]} [{line[28]}]");
+                }
+            }
+
+            Console.WriteLine("Fim");
+        }
+
+        private static void coletarDeputadosApi()
+        {
+            // https://dadosabertos.camara.leg.br/arquivos/deputados/csv/deputados.csv
+            var arquivo = @"B:\Temp\deputados.csv";
+            using var fs = File.OpenRead(arquivo);
+            using var sr = new StreamReader(fs);
+            var lines = CSVHelper.FileSplit(sr)
+                                 .Skip(1);
+            foreach (var l in lines)
+            {
+                var idLegislaturaFinal = l[3];
+
+                if (idLegislaturaFinal != "56") continue;
+
+                var uri = new Uri(l[0]);
+                var txt = FetchHelper.FetchResourceText(uri, enableCaching: true);
+                File.WriteAllText($@"B:\Temp\api\deputados\{uri.Segments[^1]}.json", txt);
+            }
+        }
+
+        static void coletarTodos()
+        {
             var spider = new SpiderCotaParlamentar(2020, 8);
             spider.Executar();
 
@@ -15,8 +75,6 @@ namespace RobopsExec
             Console.WriteLine($"Coletado: {despesas.Sum(d => d.ValorDespesa):C2} em {despesas.Length} despesas");
             var deputados = spider.ListaDeputados.ToArray();
             Console.WriteLine($" em {deputados.Length} deputados");
-
-            Console.WriteLine("Fim");
         }
     }
 }
