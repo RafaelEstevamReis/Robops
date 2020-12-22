e0f8df744a57218ad0c471b9ac9941414c888f41
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-12-22 14:58:00 -0300

    Add dados senadores

---
+++ Robops.Lib/Senado.Leg/PessoalGabinete.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Robops.Lib.Senado.Leg
+{
+    public class PessoalGabinete
+    {
+        public int CodigoFuncionario { get; set; }
+    }
+}
---
+++ Robops.Lib/Senado.Leg/Senador.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Robops.Lib.Senado.Leg
+{
+    public class Senador
+    {
+        public int CodigoSenador { get; set; }
+
+    }
+}
