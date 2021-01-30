0973fb73f71a99e5b99410048e8361c8fdc0bde1
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-01-30 12:13:23 -0300

    Criação de buferizador de dados

---
+++ Robops.Lib/Helpers/DataBuffer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Robops.Lib.Helpers
+{
+    public class DataBuffer<T>
+    {
+        public int Quantity { get; }
+        public Action<IEnumerable<T>> FlushData { get; }
+
+        List<T> queue;
+
+        public DataBuffer(int quantity, Action<IEnumerable<T>> flushData)
+        {
+            Quantity = quantity;
+            FlushData = flushData ?? throw new ArgumentNullException(nameof(flushData));
+            queue = new List<T>(quantity + 2);
+        }
+
+        public void Add(T value)
+        {
+            queue.Add(value);
+
+            if (queue.Count > Quantity)
+            {
+                Flush();
+            }
+        }
+
+        public void Flush()
+        {
+            lock (queue)
+            {
+                FlushData(queue);
+                queue.Clear();
+            }
+        }
+
+    }
+}
