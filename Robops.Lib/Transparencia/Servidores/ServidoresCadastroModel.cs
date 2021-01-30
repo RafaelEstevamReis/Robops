dc54eed0f744362f10e6ddd83839fec6c30882eb
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-01-30 12:14:03 -0300

    Leitor de cadastro de servidores

---
+++ Robops.Lib/Transparencia/Servidores/ServidoresCadastroModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Robops.Lib.Transparencia.Servidores
+{
+    public class ServidoresCadastroModel
+    {
+        public int ID_ServidorPortal { get; set; }
+        public string Nome { get; set; }
+        public string CPF_6D { get; set; }
+        public string Matricula_3D { get; set; }
+        public string DocumentoIngressoServicoPublico { get; set; }
+
+        public int CodigoOrgaoSuperiorLotacao { get; set; }
+        public int CodigoOrgaoLotacao { get; set; }
+        public int TipoVinculo { get; set; }
+        public bool ServidorCadastroMilitar { get; set; }
+    }
+}
---
+++ Robops.Spiders/Transparencia/Servidores/CarregaArquivoBaixado.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using RafaelEstevam.Simple.Spider.Helper;
+using Robops.Lib;
+using Robops.Lib.Helpers;
+using Robops.Lib.Transparencia;
+using Robops.Lib.Transparencia.Servidores;
+
+namespace Robops.Spiders.Transparencia.Servidores
+{
+    public class CarregaArquivoBaixado
+    {
+        public static void run(Simple.Sqlite.SqliteDB db)
+        {
+            db.CreateTables()
+              .Add<ServidoresCadastroModel>()
+              .Commit();
+
+            Console.WriteLine("Entre com a pasta:");
+            string pasta = Console.ReadLine();
+
+            processaPasta(pasta, db);
+        }
+        private static void processaPasta(string pasta, Simple.Sqlite.SqliteDB db)
+        {
+            foreach (var arquivo in Directory.GetFiles(pasta, "*.zip"))
+            {
+                bool militar = arquivo.Contains("_Militares");
+
+                processaArquivo(arquivo, militar, db);
+            }
+        }
+
+        private static void processaArquivo(string zipName, bool militar, Simple.Sqlite.SqliteDB db)
+        {
+            int qtd = 0;
+            var zip = new LeitorZipTransparencia(zipName);
+            zip.ShouldProcessFile = n => n.Contains("_Cadastro");
+            zip.IgnoreFirstLine = true;
+            zip.InicioLeituraArquivo += (s, a) => Console.WriteLine($"Lendo {a}");
+
+            var zipLines = zip.ReadLines();
+            var rows = CSVHelper.DelimiterSplit(zipLines, ';');
+
+            var buffer = new DataBuffer<ServidoresCadastroModel>(10000, data => db.BulkInsert(data));
+            foreach (var row in rows)
+            {
+                var cad = new ServidoresCadastroModel()
+                {
+                    ServidorCadastroMilitar = militar,
+
+                    ID_ServidorPortal = row[0].ToInt(),
+                    Nome = row[1],
+                    CPF_6D = row[2],
+                    Matricula_3D = row[3],
+                    CodigoOrgaoSuperiorLotacao = row[19].ToInt(),
+                    CodigoOrgaoLotacao = row[17].ToInt(),
+                    DocumentoIngressoServicoPublico = row[36],
+                    TipoVinculo = row[27].ToInt(),
+                };
+                buffer.Add(cad);
+                qtd++;
+
+                if (qtd % buffer.Quantity == 0) Console.WriteLine($"Processado: {qtd}");
+            }
+            buffer.Flush();
+        }
+    }
+}
