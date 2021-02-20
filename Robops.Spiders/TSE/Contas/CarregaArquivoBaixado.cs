794d885cc392361fee685233184342071f76d9c2
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-02-20 00:48:37 -0300

    Finaliza leitura receitas eleições

--- Robops.Spiders/TSE/Contas/CarregaArquivoBaixado.cs
+++ Robops.Spiders/TSE/Contas/CarregaArquivoBaixado.cs
@@ -17,30 +17,12 @@ namespace Robops.Spiders.TSE.Contas
             db.CreateTables()
               .Add<ReceitasModel>()
               .Commit();
+            string path;
 
             foreach (var file in Directory.GetFiles(@"N:\Organizando\Dados Crawlers\TSE\", "*.zip"))
             {
                 processaArquivo(db, file);
             }
-            //string path = @"N:\Organizando\Dados Crawlers\TSE\prestacao_de_contas_eleitorais_candidatos_2020.zip";
-            //processaArquivo2018_2020(db, path);
-            //string path = @"N:\Organizando\Dados Crawlers\TSE\prestacao_de_contas_eleitorais_candidatos_2018.zip";
-            //baixarArquivo_modelo2018_2020(path, db);
-            //string path = @"N:\Organizando\Dados Crawlers\TSE\prestacao_contas_final_2016.zip";
-            //processaArquivo2016(db, path);
-            //string path = @"N:\Organizando\Dados Crawlers\TSE\prestacao_final_2014.zip";
-            //processaArquivo2014(db, path);
-            //string path = @"N:\Organizando\Dados Crawlers\TSE\prestacao_contas_final_2012.zip";
-            //processaArquivo2012(db, path);
-            //string path = @"N:\Organizando\Dados Crawlers\TSE\prestacao_contas_2010.zip";
-            //processaArquivo2010(db, path);
-            //string path = @"N:\Organizando\Dados Crawlers\TSE\prestacao_contas_2008.zip";
-            //processaArquivo2008(db, path);
-            //string path = @"N:\Organizando\Dados Crawlers\TSE\prestacao_contas_2006.zip";
-            //processaArquivo2006(db, path);
-
-            //string path = @"N:\Organizando\Dados Crawlers\TSE\prestacao_contas_2004.zip";
-            //processaArquivo2004(db, path);
 
         }
 
@@ -54,6 +36,8 @@ namespace Robops.Spiders.TSE.Contas
             else if (file.EndsWith("2010.zip")) processaArquivo2010(db, file);
             else if (file.EndsWith("2008.zip")) processaArquivo2008(db, file);
             else if (file.EndsWith("2006.zip")) processaArquivo2006(db, file);
+            else if (file.EndsWith("2004.zip")) processaArquivo2004(db, file);
+            else if (file.EndsWith("2002.zip")) processaArquivo2002(db, file);
         }
 
         private static void processaArquivo2018_2020(SqliteDB db, string zipName)
@@ -69,7 +53,7 @@ namespace Robops.Spiders.TSE.Contas
                                   DocumentoDoador = row[36],
                                   NomeDoadorRFB = row[38],
                                   Valor = row[56].ToDecimal(0),
-                                  FormaPagamento = ReceitasModel.FormasPagamento.Outros,
+                                  FormaPagamento = processaFormaPagamento(row[33]),
                               });
         }
         private static void processaArquivo2016(SqliteDB db, string zipName)
@@ -85,7 +69,7 @@ namespace Robops.Spiders.TSE.Contas
                                   DocumentoDoador = row[16],
                                   NomeDoadorRFB = row[18],
                                   Valor = row[25].ToDecimal(0),
-                                  FormaPagamento = ReceitasModel.FormasPagamento.Outros,
+                                  FormaPagamento = processaFormaPagamento(row[28]),
                               });
         }
         private static void processaArquivo2014(SqliteDB db, string zipName)
@@ -101,7 +85,7 @@ namespace Robops.Spiders.TSE.Contas
                                   DocumentoDoador = row[13],
                                   NomeDoadorRFB = row[15],
                                   Valor = row[22].ToDecimal(0),
-                                  FormaPagamento = ReceitasModel.FormasPagamento.Outros,
+                                  FormaPagamento = processaFormaPagamento(row[25]),
                               });
         }
         private static void processaArquivo2012(SqliteDB db, string zipName)
@@ -117,7 +101,7 @@ namespace Robops.Spiders.TSE.Contas
                                   DocumentoDoador = row[14],
                                   NomeDoadorRFB = row[16],
                                   Valor = row[23].ToDecimal(0),
-                                  FormaPagamento = ReceitasModel.FormasPagamento.Outros,
+                                  FormaPagamento = processaFormaPagamento(row[26]),
                               });
         }
         private static void processaArquivo2010(SqliteDB db, string zipName)
@@ -133,7 +117,7 @@ namespace Robops.Spiders.TSE.Contas
                                   DocumentoDoador = row[11],
                                   NomeDoadorRFB = row[12],
                                   Valor = row[14].ToDecimal(0),
-                                  FormaPagamento = ReceitasModel.FormasPagamento.Outros,
+                                  FormaPagamento = processaFormaPagamento(row[17]),
                               });
         }
         private static void processaArquivo2008(SqliteDB db, string zipName)
@@ -149,7 +133,7 @@ namespace Robops.Spiders.TSE.Contas
                                   DocumentoDoador = row[21],
                                   NomeDoadorRFB = row[20],
                                   Valor = row[14].ToDecimal(0),
-                                  FormaPagamento = ReceitasModel.FormasPagamento.Outros,
+                                  FormaPagamento = processaFormaPagamento(row[18]),
                               });
         }
         private static void processaArquivo2006(SqliteDB db, string zipName)
@@ -167,25 +151,62 @@ namespace Robops.Spiders.TSE.Contas
                                   DocumentoDoador = row[16],
                                   NomeDoadorRFB = row[15],
                                   Valor = row[9].ToDecimal(0),
-                                  FormaPagamento = ReceitasModel.FormasPagamento.Outros,
+                                  FormaPagamento = processaFormaPagamento(row[13]),
                               });
         }
-        //private static void processaArquivo2004(SqliteDB db, string zipName)
-        //{
-        //    processaCsvZipado(db, zipName,
-        //                      n => n == "ReceitaCandidato.CSV",
-        //                      row => new ReceitasModel()
-        //                      {
-        //                          Ano = 2004,
-        //                          CodigoEleicao = 0,
-        //                          UF = row[5],
-        //                          NumeroCandidato = row[4].ToInt(),
-        //                          DocumentoDoador = row[16],
-        //                          NomeDoadorRFB = row[15],
-        //                          Valor = row[9].ToDecimal(0),
-        //                          FormaPagamento = ReceitasModel.FormasPagamento.Outros,
-        //                      });
-        //}
+        private static void processaArquivo2004(SqliteDB db, string zipName)
+        {
+            processaCsvZipado(db, zipName,
+                              n => n == "ReceitaCandidato.CSV",
+                              row => new ReceitasModel()
+                              {
+                                  Ano = 2004,
+                                  CodigoEleicao = 0,
+                                  UF = row[4],
+                                  NumeroCandidato = row[3].ToInt(),
+                                  DocumentoDoador = row[16],
+                                  NomeDoadorRFB = row[15],
+                                  Valor = row[9].ToDecimal(0),
+                                  FormaPagamento = processaFormaPagamento(row[13]),
+                              });
+        }
+        private static void processaArquivo2002(SqliteDB db, string zipName)
+        {
+            // O arquivo original baixado está com o ZIP corrompido
+            // mas pode ser aberto e resalvo
+            processaCsvZipado(db, zipName,
+                              n => n == "ReceitaCandidato.csv",
+                              row => new ReceitasModel()
+                              {
+                                  Ano = 2002,
+                                  CodigoEleicao = 0,
+                                  UF = row[1],
+                                  NumeroCandidato = row[5].ToInt(),
+                                  DocumentoDoador = row[7],
+                                  NomeDoadorRFB = row[9],
+                                  Valor = row[10].ToDecimal(0),
+                                  FormaPagamento = processaFormaPagamento(row[11]),
+                              });
+        }
+
+        private static ReceitasModel.FormasPagamento processaFormaPagamento(string forma)
+        {
+            // ordenado por frequência
+            if (forma == "Estimado") return ReceitasModel.FormasPagamento.Estimado;
+            if (forma.ToLower().Contains("dep")) return ReceitasModel.FormasPagamento.Deposito;
+            if (forma.ToLower().Contains("transf")) return ReceitasModel.FormasPagamento.Transferencia;
+            if (forma == "Em espécie") return ReceitasModel.FormasPagamento.Dinheiro;
+            if (forma == "Cheque") return ReceitasModel.FormasPagamento.Cheque;
+            if (forma.ToLower().Contains("crédito")) return ReceitasModel.FormasPagamento.CCredito;
+            if (forma.ToLower().Contains("boleto")) return ReceitasModel.FormasPagamento.Boleto;
+            if (forma.ToLower().Contains("débito")) return ReceitasModel.FormasPagamento.CDebito;
+            if (forma.ToLower() == "não informado") return ReceitasModel.FormasPagamento.Outros;
+            if (forma.ToLower().Contains("outros t")) return ReceitasModel.FormasPagamento.Outros;
+            if (forma.StartsWith("--")) return ReceitasModel.FormasPagamento.Outros;
+            if (forma== "Despesa não quitada") return ReceitasModel.FormasPagamento.Outros;
+            
+            return ReceitasModel.FormasPagamento.Outros;
+        }
 
         private static void processaCsvZipado(SqliteDB db, string zipName, Func<string, bool> fileFilter, Func<string[], ReceitasModel> extractionFilter)
         {
@@ -203,7 +224,7 @@ namespace Robops.Spiders.TSE.Contas
             });
             int qtd = 0;
             foreach (var row in rows)
-            {                
+            {
                 var cad = extractionFilter(row);
                 buffer.Add(cad);
 
@@ -212,5 +233,7 @@ namespace Robops.Spiders.TSE.Contas
             }
             buffer.Flush();
         }
+
+
     }
 }
