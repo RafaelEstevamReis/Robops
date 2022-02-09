679ac62ec3751b53424e5cbe53b51a308a9ace9c
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-02-08 22:24:18 -0300

    Update ColetarVerba.cs

--- Robops.Spiders/AL/MG/ColetarVerba.cs
+++ Robops.Spiders/AL/MG/ColetarVerba.cs
@@ -9,49 +9,54 @@ namespace Robops.Spiders.AL.MG
     {
         public static void run(int ano)
         {
+            //if (ano != 2020) return;
+
             var deputados = FetchHelper.FetchResourceXml<ListaDeputado>(new Uri("http://dadosabertos.almg.gov.br/ws/deputados/em_exercicio"), enableCaching: true);
 
-            using var fs = File.OpenWrite("almg_gastos.csv");
-            var sw = new StreamWriter(fs);
-
-            foreach (var d in deputados.deputados)
+            //using var fs = File.Open($"almg_gastos_{ano}.csv", FileMode.Append, FileAccess.Write);
+            using (var fs = File.Open($"almg_gastos.csv", FileMode.Append, FileAccess.Write))
             {
-                for (int mes = 1; mes <= 12; mes++)
+                using var sw = new StreamWriter(fs);
+
+                foreach (var d in deputados.deputados)
                 {
-                    // http://dadosabertos.almg.gov.br/ws/prestacao_contas/verbas_indenizatorias/deputados/7753/2012/1
-                    //string url = $"http://dadosabertos.almg.gov.br/ws/prestacao_contas/verbas_indenizatorias/legislatura_atual/deputados/{d.id}/{ano}/{mes}";
-                    string url = $"http://dadosabertos.almg.gov.br/ws/prestacao_contas/verbas_indenizatorias/deputados/{d.id}/{ano}/{mes}";
+                    for (int mes = 1; mes <= 12; mes++)
+                    {
+                        // http://dadosabertos.almg.gov.br/ws/prestacao_contas/verbas_indenizatorias/deputados/7753/2012/1
+                        //string url = $"http://dadosabertos.almg.gov.br/ws/prestacao_contas/verbas_indenizatorias/legislatura_atual/deputados/{d.id}/{ano}/{mes}";
+                        string url = $"http://dadosabertos.almg.gov.br/ws/prestacao_contas/verbas_indenizatorias/deputados/{d.id}/{ano}/{mes}";
 
-                    ListaResumoVerba gastos;
-                    try
-                    {
-                        gastos = FetchHelper.FetchResourceXml<ListaResumoVerba>(new Uri(url), enableCaching: true);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                        System.Threading.Thread.Sleep(2000); // delay for avoid 429
-                        continue;
-                    }
-                    //System.Threading.Thread.Sleep(1000); // 1s delay for avoid 429
-
-                    if (gastos.resumoVerba == null)
-                    {
-                        Console.WriteLine("NLL");
-                        continue;
-                    }
-
-                    foreach(var despesa in gastos.resumoVerba)
-                    {
-                        foreach(var conta in despesa.listaDetalheVerba)
+                        ListaResumoVerba gastos;
+                        try
                         {
-                            sw.WriteLine($"{d.nome};{ano};{mes};{despesa.descTipoDespesa};{conta.dataEmissao:yyyy-MM-dd};{conta.cpfCnpj};{conta.valorDespesa};{conta.valorReembolsado};{conta.nomeEmitente}");
+                            gastos = FetchHelper.FetchResourceXml<ListaResumoVerba>(new Uri(url), enableCaching: true);
                         }
-                    }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            System.Threading.Thread.Sleep(2000); // delay for avoid 429
+                            continue;
+                        }
+                        //System.Threading.Thread.Sleep(1000); // 1s delay for avoid 429
 
+                        if (gastos.resumoVerba == null)
+                        {
+                            Console.WriteLine("NLL");
+                            continue;
+                        }
+
+                        foreach (var despesa in gastos.resumoVerba)
+                        {
+                            foreach (var conta in despesa.listaDetalheVerba)
+                            {
+                                sw.WriteLine($"{d.nome};{ano};{mes};{despesa.descTipoDespesa};{conta.dataEmissao:yyyy-MM-dd};{conta.cpfCnpj};{conta.valorDespesa};{conta.valorReembolsado};{conta.nomeEmitente}");
+                            }
+                        }
+                        sw.Flush();
+                    }
                 }
             }
-
+            Console.WriteLine("Fim ANO");
         }
     }
 }
