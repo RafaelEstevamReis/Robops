6cb9f0daa4258c61985b7fbc149f2a6db3c25c0d
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-31 08:54:12 -0300

    Adiciona esqueleto

---
+++ Robops.Pickle/Interfaces/IColeta.cs
@@ -0,0 +1,12 @@
+﻿using Robops.Pickle.Models;
+
+namespace Robops.Pickle.Interfaces
+{
+    public interface IColeta
+    {
+        public string NomeCasa { get; }
+
+        void Setup(ConfiguracaoColeta configuracao);
+        public void ColetarDados();
+    }
+}
