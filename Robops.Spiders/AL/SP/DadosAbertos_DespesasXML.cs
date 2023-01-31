1373cc8bfe6e322e7a5379a42952f2a666877078
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-31 07:59:36 -0300

    Update DadosAbertos_DespesasXML.cs

--- Robops.Spiders/AL/SP/DadosAbertos_DespesasXML.cs
+++ Robops.Spiders/AL/SP/DadosAbertos_DespesasXML.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using RafaelEstevam.Simple.Spider.Helper;
 using System.IO;
 using System.Text;
-using RafaelEstevam.Simple.Spider.Helper;
 
 namespace Robops.Spiders.AL.SP
 {
