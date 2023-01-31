1b120804bf342a5510085dbda3012d1b6cafc027
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-31 09:06:20 -0300

    Boot da aplicação

--- Robops.Pickle/Models/DadosColeta.cs
+++ Robops.Pickle/Models/DadosColeta.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Serilog;
 
 namespace Robops.Pickle.Models
 {
     public class ConfiguracaoColeta
     {
+        public ILogger Logger { get; set; }
     }
 }
--- Robops.Pickle/Program.cs
+++ Robops.Pickle/Program.cs
@@ -1,7 +1,35 @@
-﻿using System;
+﻿using Robops.Pickle.Interfaces;
+using Robops.Pickle.Models;
+using Serilog;
+using Serilog.Events;
+using Simple.BotUtils.Helpers;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
-Console.WriteLine("Hello, World!");
+var cfg = new ConfiguracaoColeta()
+{
+    Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.File(Path.Combine("logs", "Logs.txt"), rollingInterval: RollingInterval.Day)
+            .CreateLogger(),
+};
+
+cfg.Logger.Information("Pickle Start");
 
 // Listar todos os coletores
+var types = TypeHelper.GetClassesOfType<IColeta>(Assembly.GetExecutingAssembly());
+var coletores = TypeHelper.CreateInstancesFor<IColeta>(types)
+                          .ToArray();
+cfg.Logger.Information($"{coletores.Length} coletores inicializados");
 
+foreach (var coletor in coletores)
+{
+    cfg.Logger.Information($"[INIT] {coletor.GetType().Name} para {coletor.NomeCasa} ");
+    coletor.Setup(cfg);
+    coletor.ColetarDados();
+}
 
+cfg.Logger.Information("Finalizado");
